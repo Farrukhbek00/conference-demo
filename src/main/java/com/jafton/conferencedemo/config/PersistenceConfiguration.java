cd4a274c7e82de7297793a2ddccc0b1165e511c2
Farrukhbek <farruhzokirov00@gmail.com>
2020-04-09 12:55:14 +0500

    database configuration for heroku

--- src/main/java/com/jafton/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/jafton/conferencedemo/config/PersistenceConfiguration.java
@@ -1,10 +1,6 @@
 package com.jafton.conferencedemo.config;
 
-import org.springframework.boot.jdbc.DataSourceBuilder;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
@@ -13,13 +9,13 @@ public class PersistenceConfiguration {
      or create changes or configuration in our persistence
      add @Configuration annotation*/
 
-     @Bean
-     public DataSource dataSource() {
-         DataSourceBuilder builder = DataSourceBuilder.create();
-         builder.password("1234");
-         builder.username("postgres");
-         builder.url("jdbc:postgresql://localhost:5432/conference_app");
-         System.out.println("My custom datasource bean has been initialized and set");
-         return builder.build();
-     }
+//     @Bean
+//     public DataSource dataSource() {
+//         DataSourceBuilder builder = DataSourceBuilder.create();
+//         builder.password("1234");
+//         builder.username("postgres");
+//         builder.url("jdbc:postgresql://localhost:5432/conference_app");
+//         System.out.println("My custom datasource bean has been initialized and set");
+//         return builder.build();
+//     }
 }

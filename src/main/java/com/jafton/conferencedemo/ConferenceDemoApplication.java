d84e7eb86dc40bd426569971ad1048216f2dea83
Farrukhbek <farruhzokirov00@gmail.com>
2020-04-09 11:55:10 +0500

    fixes

--- src/main/java/com/jafton/conferencedemo/ConferenceDemoApplication.java
+++ src/main/java/com/jafton/conferencedemo/ConferenceDemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ConferenceDemoApplication {
 
+    // Run
     public static void main(String[] args) {
         SpringApplication.run(ConferenceDemoApplication.class, args);
     }
